b5664388a258c24e4afbe1093537f54fbb20edcb
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-12-26 21:58:06 +0530

    redis issue fixed

--- src/main/java/in/kafka/redis/model/Language.java
+++ src/main/java/in/kafka/redis/model/Language.java
@@ -1,10 +1,12 @@
 package in.kafka.redis.model;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @lombok.Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Language {
+public class Language implements Serializable {
 	private String iso639_1;
 	private String iso639_2;
 	private String name;
